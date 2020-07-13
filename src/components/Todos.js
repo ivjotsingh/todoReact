ba69a3170c1e92d10b6089af30d82f4cb32b680b
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-07-14 02:30:47 +0530

    todos component imported in APP

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class Todos extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>Todos</h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
