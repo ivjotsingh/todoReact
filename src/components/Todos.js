efb882aedc442a58fb1653e65f64feeef04d5461
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-07-15 16:36:11 +0530

    PropTypes added in Todos component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import TodoItem from '../components/TodoItem'
+import TodoItem from '../components/TodoItem';
+import PropTypes from 'prop-types';
 
 
 class Todos extends Component{
@@ -7,10 +8,16 @@ class Todos extends Component{
   render(){
     return (
       this.props.todos.map((todo) => (
-        < TodoItem todo={todo}/>
+        < TodoItem key = {todo.id} todo={todo}/>
     ))
     );
   }
 }
+
+// proptypes
+  Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+  }
+
 
 export default Todos;
