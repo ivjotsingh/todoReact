8369d04f9ae2e7fc1fcc755d8071d591031adec0
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-07-16 00:25:23 +0530

    added basic css to TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,15 +3,32 @@ import PropTypes from 'prop-types';
 
 
 class TodoItem extends Component{
+
+getItemStyle = () => {
+  return {
+    backgroundColor: '#f4f4f4',
+    padding: '10px',
+    borderBottom: '1px #ccc dotted',
+    textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+  }
+}
   
   render(){
+    console.log(this.props.todo.completed)
     return (
+      <div style={this.getItemStyle()}>
         <h3> {this.props.todo.title} </h3>
+      </div>
     );
   }
 }
+
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const itemStyle = {
+    
 }
 
 export default TodoItem;
