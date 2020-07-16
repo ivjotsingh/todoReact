c7ea6a0ac0361910f2b06b5cf606f0ac46bc313e
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-07-16 18:12:33 +0530

    layout folder added to store CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todos from './components/Todos'
+import Header from './components/layout/Header'
 import './App.css';
 
 class App extends Component{
@@ -38,7 +39,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <h1>APP</h1>
+        <Header/>
         <Todos todos={this.state.todos} markComplete = {this.markComplete}/>
       </div>
     );
